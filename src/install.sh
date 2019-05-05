018e076dd0e776595cfdd4c7c880c2695bfaff73
SirScythe <8879605+SirScythe@users.noreply.github.com>
2019-05-05 18:37:18 +0200

    centralized install file for supervisord

---
+++ src/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /conanexiles +app_update 443030 validate +$UPDATE_MODS +quit
+
+for mod in $(ls /root/Steam/steamapps/workshop/content/440900/)
+do
+  ls /root/Steam/steamapps/workshop/content/440900/$mod/
+  ls /root/Steam/steamapps/workshop/content/440900/$mod/*
+done
